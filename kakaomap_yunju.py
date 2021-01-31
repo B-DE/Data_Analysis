7d1f6d89a875b74f6e8d1b19fd976d54ba40c3ab
yunju-kang <daria0155@naver.com>
2021-02-01 02:14:16 +0900

    create explanation of variable

--- kakaomap_yunju.py
+++ kakaomap_yunju.py
@@ -26,17 +26,10 @@ with open(file_path) as json_file:
 
 
 
-## 데이터 형식 변경
-
-### 데이터프레임 형식
-df = pd.DataFrame(json_data)
-
-### 리스트형식으로 게시글 저장
-name = []
+## 리스트형식으로 게시글 저장
+nameData = []
 for i in json_data:
-    data.append(df[i]['name'])
-
-
+    nameData.append(i)
 
 
 
@@ -44,48 +37,62 @@ for i in json_data:
 # 드라이버 연결
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+
+
 ## 드라이버 선택
 driver = webdriver.Chrome('C:/Project/chromedriver.exe')
+
+
 
 ## 카카오맵 url 연결
 driver.get('https://map.kakao.com/')
 
-##
 
 
-
-
-
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# 주소 가져오기
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-
-
-
-
-## 카카오맵 분석
-
-driver = webdriver.Chrome('C:/instachatbot/chromedriver_win32 (2)/chromedriver.exe')
-driver.get('https://map.kakao.com/')
-time.sleep(2)
-
-# 검색 창
-search_area = driver.find_element_by_xpath('//*[@id="search.keyword.query"]')
-
-# 검색어 입력
-search_area.send_keys('클랭블루')
-
-# Enter로 검색
-driver.find_element_by_xpath('//*[@id="search.keyword.submit"]').send_keys(Keys.ENTER)  # Enter로 검색
-time.sleep(1)
-
+## BeautifulSoup 설정
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-cafe_lists = soup.select('.placelist > .PlaceItem')
-for cafe in cafe_lists:
-    cafe_address = cafe.select('div.addr > p ')[0].text  # cafeName
+
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# 카페명 검색
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+address = []
+bigRegion = []
+smallRegion = []
+industry = []
+phone = []
+
+
+for i in nameData:
+
+    ## 카페명 변수 설정
+    searchName = i
+
+
+    ## 검색어 입력
+    search_area = driver.find_element_by_xpath('//*[@id="search.keyword.query"]')
+    search_area.send_keys(i)
+
+
+    ## Enter로 검색
+    driver.find_element_by_xpath('//*[@id="search.keyword.submit"]').send_keys(Keys.ENTER)  # Enter로 검색
+    time.sleep(1)
+
+
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    # 주소 크롤링
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+    cafe_lists = soup.select('.placelist > .PlaceItem')
+    for cafe in cafe_lists:
+        cafe_address = cafe.select('div.addr > p ')[0].text
+
+
 
 # 결과값 확인
 print(cafe_address)
