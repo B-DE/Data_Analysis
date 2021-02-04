b1502725c742626edeca4e01dcaad7c6a8717953
yunju-kang <daria0155@naver.com>
2021-02-05 03:42:07 +0900

    numbeer crawling

--- kakaomap_yunju.py
+++ kakaomap_yunju.py
@@ -85,74 +85,149 @@ for i in nameData:
 
 
 
+
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    # 데이터 크롤링
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+
     ## BeautifulSoup 설정
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
 
 
-    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    # 주소 크롤링
-    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-
+    ## 검색어 접근을 위한 숫자 변수
     count = int(0)
-    cafe_list = soup.select('.placelist > .PlaceItem')
+
+
+
+    ## 태그 접근
+    cafe_list = soup.select('.placelist  .PlaceItem')
     for cafe in cafe_list:
 
 
-
-        ## 태그 접근
+        ### 검색된 업종명에 접근
         try:
             searched = soup.select('.PlaceItem.clickArea .link_name')[count].text
             print(searched)
 
 
-            ## 주소 저장(일치하는 게시물에 대해) (!!!! 두번째 이상의 게시물 주소 크롤링)
-            if(i == searched):
-                cafe_address = cafe.select('.PlaceItem.clickArea .addr > p')[0].text
-                address.append(cafe_address)
-                print(cafe_address)
-                break
 
 
-            ## 검색명과 일치하는 게시물이 없을 시
-            else:
+            '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+            # 주소 크롤링
+            '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+            try:
 
-                ## 마지막 게시물이 아니면 반복 진행
-                if(count <= len(soup.select('.PlaceItem.clickArea .link_name'))):
-                    print('next')
-
-
-                ## 마지막 게시물이라면 공백 저장 후 반복 종료
-                else:
-                    address.append('')
-                    print('break')
+                ## 주소 저장 (일치하는 게시물에 대해)
+                if(i == searched):
+                    cafe_address = cafe.select('.PlaceItem.clickArea .addr > p')[0].text
+                    address.append(cafe_address)
+                    print(cafe_address)
                     break
 
 
-        ## 태그가 없을 시 주소 공백으로 저장
+
+                ## 검색명과 일치하는 게시물이 없을 시
+                else:
+
+
+
+                    ### 마지막 게시물이 아니면 반복 진행
+                    if(count <= len(cafe.select('.PlaceItem.clickArea .link_name'))):
+                        print('address next')
+
+
+
+                    ### 마지막 게시물이라면 공백 저장 후 반복 종료
+                    else:
+                        address.append('')
+                        print('address break')
+                        break
+
+
+            except:
+                print('address error')
+                address.append('')
+
+
+
+            '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+            # 전화번호 크롤링
+            '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+            try:
+
+
+                ## 번호 저장 (일치하는 게시물에 대해)
+                if (i == searched):
+                    cafe_phone = cafe.select('.PlaceItem.clickArea .phone')[0].text
+                    phone.append(cafe_phone)
+                    print(cafe_phone)
+                    break
+
+
+                ## 검색명과 일치하는 게시물이 없을 시
+                else:
+
+
+                    ### 마지막 게시물이 아니면 반복 진행
+                    if (count <= len(cafe.select('.PlaceItem.clickArea .link_name'))):
+                        print('phone next')
+
+
+                    ### 마지막 게시물이라면 공백 저장 후 반복 종료
+                    else:
+                        phone.append('')
+                        print('phone break')
+                        break
+
+
+            except:
+                print('phone error')
+                phone.append('')
+
+
+
+
+
+
+        ### 태그가 없을 시 모든 데잍터를 공백으로 저장
         except:
             address.append('')
+            phone.append('')
+            industry.append('')
             print('error')
             break
 
+
+
+
+
+        ## 숫자변수 증가 (다음 반복 실행시 searched변수를 변경하기 위해))
         count += 1
+
 
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # 마무리
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+
 ## 드라이버 종료
 driver.close()
 driver.quit()
 
 
-## 결과값 확인
-print(address)
-for i in address:
-    print(i + "\n\n")
 
+## 결과값 확인t(address)
+for i in phone:
+    print(i)
+
+# prin
 
